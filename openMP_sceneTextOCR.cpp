d6ec69ef83003459560f418e2bb0267068247eb2
yge58 <yge58@uwo.ca>
2017-08-16 16:02:34 -0400

    Update openMP_sceneTextOCR.cpp

--- openMP_sceneTextOCR.cpp
+++ openMP_sceneTextOCR.cpp
@@ -14,7 +14,7 @@
 // Third party copyrights are property of their respective owners.
 /////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////
- * (C) Copyright 2006, Google Inc.
+ /* (C) Copyright 2006, Google Inc.
  ** Licensed under the Apache License, Version 2.0 (the "License");
  ** you may not use this file except in compliance with the License.
  ** You may obtain a copy of the License at
@@ -24,7 +24,7 @@
  ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  ** See the License for the specific language governing permissions and
  ** limitations under the License.
- *
+ */
 ///////////////////////////////////////////////////////////////////////////////
 #include <omp.h>
 // CPP
