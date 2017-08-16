ff90b91b07705a93fa3acc9f984d3f99d51d0f41
yge58 <yge58@uwo.ca>
2017-08-16 16:02:49 -0400

    Update sceneTextOCR.cpp

--- sceneTextOCR.cpp
+++ sceneTextOCR.cpp
@@ -15,7 +15,7 @@
 /////////////////////////////////////////////////////////////////////
 
 /////////////////////////////////////////////////////////////////////////////
- * (C) Copyright 2006, Google Inc.
+ /* (C) Copyright 2006, Google Inc.
  ** Licensed under the Apache License, Version 2.0 (the "License");
  ** you may not use this file except in compliance with the License.
  ** You may obtain a copy of the License at
@@ -25,7 +25,7 @@
  ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  ** See the License for the specific language governing permissions and
  ** limitations under the License.
- *
+ */
 ///////////////////////////////////////////////////////////////////////////////
 // CPP
 #include <iostream>
